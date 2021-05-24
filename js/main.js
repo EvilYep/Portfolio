5ff0a77aece4fe675af2a381ec98f7be262754a3
EvilYep <guillaume.moneret@gmail.com>
2021-05-24 21:45:46 +0200

    removed repeating background cos' too hard to handle with resizes :coffin:

--- js/main.js
+++ js/main.js
@@ -2,17 +2,12 @@ let ctx = document.getElementById('my-canvas').getContext('2d');
 let buffer = document.createElement("canvas").getContext('2d');
 let cW = document.documentElement.clientWidth - GLOBAL_MARGIN;
 let cH = document.documentElement.clientHeight - GLOBAL_MARGIN;
-let speed = 0;
 let distance = INITIAL_DISTANCE;
 let tileSize = LEVEL_TILE_SIZES.LG;
 let bufferMaxSize = tileSize * COLS;
 
-let status = 'idle';
-let facing = 'right';
-let maxFrame = 4;
-let currentFrame = 0;
-let currentObjectFrame = 0;
-let middleOfTheRoad = 0;
+let status = 'idle', facing = 'right';
+let speed = 0, maxFrame = 4, currentFrame = 0, currentObjectFrame = 0, middleOfTheRoad = 0;
 
 function checkUndesirableResizes() {
     tileSize = cH < 700 ? LEVEL_TILE_SIZES.SM : LEVEL_TILE_SIZES.LG;
@@ -24,52 +19,23 @@ function resetCoords() {
 }
 
 function refreshSmoothies() {
-    ctx.mozImageSmoothingEnabled = false;
-    ctx.webkitImageSmoothingEnabled = false;
-    ctx.msImageSmoothingEnabled = false;
-    ctx.imageSmoothingEnabled = false;
-    buffer.mozImageSmoothingEnabled = false;
-    buffer.webkitImageSmoothingEnabled = false;
-    buffer.msImageSmoothingEnabled = false;
-    buffer.imageSmoothingEnabled = false;
+    ctx.mozImageSmoothingEnabled = false, ctx.webkitImageSmoothingEnabled = false, ctx.msImageSmoothingEnabled = false, ctx.imageSmoothingEnabled = false;
+    buffer.mozImageSmoothingEnabled = false, buffer.webkitImageSmoothingEnabled = false, buffer.msImageSmoothingEnabled = false, buffer.imageSmoothingEnabled = false;
 }
 
 function BackGround() {
-    //resetCoords();
-    this.x = 0;
-    this.x2 = 0;
-    this.x3 = 0; 
-    this.x4 = 0;
-    this.xRoad = 0;
-    this.y = 0; 
-    this.scaling = cW / 0.63;
+    this.x = 0, this.x2 = 0, this.x3 = 0, this.x4 = 0, this.xRoad = 0, this.y = 0; 
+    this.scaling = cW / 0.5;
     
     // Dark magic revolving around unusual background images resolutions and an initial idea of a fixed 'display' window size
     // And since there's NO WAY I'm changing these sweets free assets, wud'ov tak' me yoars t'do doz, mate !.....
     this.render = function() {
         ctx.drawImage(bg5, 0, 0);
-        ctx.drawImage(bg4, this.x4-=speed, 0, this.scaling, cH / 1.85);
+        ctx.drawImage(bg4, this.x4-=speed*1.2, 0, this.scaling, cH / 1.85);
         ctx.drawImage(bg3, this.x3-=(speed*2), cH / 12, this.scaling, cH / 1.85);
         ctx.drawImage(bg2, this.x2-=(speed*2.5), cH / 8, this.scaling, cH / 1.85);
-        ctx.drawImage(bg, this.x-=(speed*3), cH / 6.3, this.scaling, cH / 1.85);
+        ctx.drawImage(bg, this.x-=(speed*3), cH / 6.3, this.scaling * 1.1, cH / 1.85);
         ctx.drawImage(road, this.xRoad-=(speed*6), cH / 1.48, cW * 7, cH / 3);
-
-        middleOfTheRoad = (cH + (cH / 1.48)) / 2 ;
-        //console.log(middleOfTheRoad);
-
-        if((this.x <= -(this.scaling/3 - 1)) && status == 'running' && speed == 1) { this.x = 0; }
-        if((this.x2 <= -(this.scaling/3 - 1)) && status == 'running' && speed == 1) { this.x2 = 0; }
-        if((this.x3 <= -(this.scaling/3 - 1)) && status == 'running' && speed == 1) { this.x3 = 0; }
-        if((this.x4 <= -(this.scaling/3 - 1)) && status == 'running' && speed == 1) { this.x4 = 0; }
-
-        if((this.x + this.scaling >= cW + (this.scaling/3 -1)) && status == 'running' && speed == -1) { this.x = cW - this.scaling; }
-        if((this.x2 + this.scaling >= cW + (this.scaling/3 -1)) && status == 'running' && speed == -1) { this.x2 = cW - this.scaling; }
-        if((this.x3 + this.scaling >= cW + (this.scaling/3 -1)) && status == 'running' && speed == -1) { this.x3 = cW - this.scaling; }
-        if((this.x4 + this.scaling >= cW + (this.scaling/3 -1)) && status == 'running' && speed == -1) { this.x4 = cW - this.scaling; }
-
-        if(this.xRoad <= -(cW * 3.49) || this.xRoad >= (cW * 3.49)) { this.xRoad = 0; }
-
-        // Pfiiiiiooooouuuuu
     }
 }
 
@@ -78,7 +44,6 @@ function ForeGround(resArray) {
     this.marginB = 2/100 * cH; 
     
     this.render = function() {
-        
         buffer.canvas.width = tileSize * COLS;
         buffer.canvas.height = tileSize * ROWS;
         
@@ -106,7 +71,6 @@ function ForeGround(resArray) {
             if (value == 'P') {
                 buffer.drawImage(this.imgs.Screen2, currentObjectFrame * 32, 0, 32, 42, tile_x, tile_y - 5, tileSize - 40, tileSize);
             }
-            //buffer.drawImage(tile_sheet, value * DUDES_TILE_SIZE, 0, DUDES_TILE_SIZE, DUDES_TILE_SIZE, tile_x, tile_y, LEVEL_TILE_SIZE, LEVEL_TILE_SIZE);
         }        
         ctx.drawImage(buffer.canvas, distance - INITIAL_DISTANCE, buffer.canvas.height - cH, cW, cH, 0, -this.marginB, cW, cH);
     }
@@ -114,41 +78,41 @@ function ForeGround(resArray) {
 
 function Player(resArray) {
     this.imgs = resArray;
-    let me = this;
 
     this.render = function() {
         refreshSmoothies();
+        middleOfTheRoad = (cH + (cH / 1.48)) / 2 ;
         switch(status) {
             case 'idle':
-                this.sprite = me.imgs.Biker_idle;
+                this.sprite = this.imgs.Biker_idle;
                 maxFrame = 4;
                 break;
             case 'running':
-                if(facing == 'left') {
-                    this.sprite = me.imgs.Biker_run_L;
-                } else {
-                    this.sprite = me.imgs.Biker_run;
-                }
+                this.sprite = facing == 'left' ? this.imgs.Biker_run_L : this.imgs.Biker_run;
                 maxFrame = 6;
                 break;
             default:
-                this.sprite = me.imgs.Biker_idle;
+                this.sprite = this.imgs.Biker_idle;
                 maxFrame = 4;
                 break;
         }
-        ctx.drawImage(this.sprite, currentFrame * DUDES_TILE_SIZE, 0, DUDES_TILE_SIZE, DUDES_TILE_SIZE, INITIAL_DISTANCE, middleOfTheRoad - 190, 190, 190);
+        ctx.drawImage(this.sprite, currentFrame * DUDES_TILE_SIZE, 0, DUDES_TILE_SIZE, DUDES_TILE_SIZE, INITIAL_DISTANCE, middleOfTheRoad - 210, 210, 210);
     }
 }
 
 function run(direction) {
     speed = direction;
-    if(direction == -1) {
-        facing = 'left';
-    } else {
-        facing = 'right';
-    }
+    facing = direction == -1 ? 'left' : 'right'
     status = 'running';
     currentFrame = 0;
+}
+
+function runAwayYouFools(direction) {
+    stop();
+    run(direction);
+    setTimeout(function() {
+        stop();
+    }, 1000);
 }
 
 function stop() {
@@ -158,7 +122,22 @@ function stop() {
     currentFrame = 0;
 }
 
+// Credits to this guy for this one ! https://github.com/shubhamjain/penguin-walk
+function loadResources(imgPaths, whenLoaded) {
+	let imgs = {}, imgCounter = 0;
+  	imgPaths.forEach(function(path){
+		let img = document.createElement('img');
+		img.src = path;
+		let fileName = path.split(/[\./]/).slice(-2, -1)[0];
+		img.onload = function(){
+			imgs[fileName] = img;
+			imgCounter++;
+			imgPaths.length == imgCounter ? whenLoaded(imgs) : console.log('tremendous loading time...');	
+		};
+	});
+}
 
+// MAIN LOOP FUNCTION
 
 function initCanvas(resArray) {
     let background = new BackGround();
@@ -168,8 +147,6 @@ function initCanvas(resArray) {
     function animateGlobal() {
         ctx.save();
         ctx.clearRect(0, 0, cW, cH);
-
-        // Start drawing
         resetCoords();
         checkUndesirableResizes();
         ctx.canvas.width = cW;
@@ -178,98 +155,42 @@ function initCanvas(resArray) {
         background.render();
         foreground.render();
         player.render();
-
-        if(speed == 1) {
-            distance += 6;    
-        }
-        if(speed == -1) {
-            distance -= 6; 
-        }
+ 
+        speed != 0 ? distance = distance + (Math.sign(speed)*6) : distance;
         if(distance < INITIAL_DISTANCE) {
-            stop();
-            run(1);
-            setTimeout(function() {
-                stop();
-            }, 2000);
+            runAwayYouFools(1);
         }
         if(distance >= bufferMaxSize) {
-            stop();
-            run(-1);
-            setTimeout(function() {
-                stop();
-            }, 2000);
+            runAwayYouFools(-1);
         }
-                
-        // End drawing
 
         ctx.restore();
     }
 
     function animateSprites() {
-        currentFrame++;
-        currentObjectFrame++;
-        if(currentFrame >= maxFrame) {
-            currentFrame = 0;
-        }
-        if(currentObjectFrame >= 4) {
-            currentObjectFrame = 0;
-        }
+        currentFrame++, currentObjectFrame++;
+        currentFrame = currentFrame >= maxFrame ? 0 : currentFrame;
+        currentObjectFrame = currentObjectFrame >= 4 ? 0 : currentObjectFrame  
     }
 
     setInterval(animateGlobal, 30);
-    setInterval(animateSprites, 100);
+    setInterval(animateSprites, 150);
 }
 
-// Credits to this guy for this one !
-// https://github.com/shubhamjain/penguin-walk
-function loadResources(imgPaths, whenLoaded)
-{
-	var imgs = {}, imgCounter = 0;
-
-  	imgPaths.forEach(function(path){
-		var img = document.createElement('img');
-		img.src = path;
-
-		var fileName = path.split(/[\./]/).slice(-2, -1)[0];
-		img.onload = function(){
-			imgs[fileName] = img;
-			imgCounter++;
-
-			if (imgPaths.length == imgCounter)
-			{
-                whenLoaded(imgs);	
-			}
-		};
-	});
-}
+// LET's GOOOOOOO !
 
 window.addEventListener('load', function(e) {
     loadResources([
-        "./assets/bg/5.png",
-        "./assets/bg/4.png",
-        "./assets/bg/3.png",
-        "./assets/bg/2.png",
-        "./assets/bg/1.png",
-        "./assets/bg/road.png",
-        "./assets/player/Biker_hurt.png",
-        "./assets/player/Biker_idle.png",
-        "./assets/player/Biker_run.png",
-        "./assets/player/Biker_run_L.png",
-        "./assets/objects/Pointer1.png",
-        "./assets/objects/Pointer2.png",
-        "./assets/objects/Fence1.png",
-        "./assets/objects/Fence2.png",
-        "./assets/objects/Fence3.png",
+        "./assets/bg/5.png", "./assets/bg/4.png", "./assets/bg/3.png", "./assets/bg/2.png", "./assets/bg/1.png", "./assets/bg/road.png",
+        "./assets/player/Biker_hurt.png", "./assets/player/Biker_idle.png", "./assets/player/Biker_run.png", "./assets/player/Biker_run_L.png",
+        "./assets/objects/Pointer1.png", "./assets/objects/Pointer2.png",
+        "./assets/objects/Fence1.png", "./assets/objects/Fence2.png", "./assets/objects/Fence3.png",
         "./assets/objects/Screen2.png",
     ], initCanvas);
 });
 
 window.addEventListener('mousedown', function(e) {
-    if(INITIAL_DISTANCE - e.clientX > 0) {
-        run(-1);
-    } else {
-        run(1);
-    }
+    INITIAL_DISTANCE - e.clientX > 0 ? run(-1) : run(1);
 })
 
 window.addEventListener('mouseup', function(e) {
